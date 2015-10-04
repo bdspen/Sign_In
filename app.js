eae855ee7ec067bba0e6da6b5741fd7301878aed
Ben <bdspen@gmail.com>
2015-10-04 15:23:48 -0700

    add bootstrap theme yeti and fix some styling issues, remove home page

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var signIn = angular.module('signIn', ['ui.router']);
 signIn.config(function($stateProvider, $urlRouterProvider){
   $stateProvider.state('home',{
     url:"",
-    templateUrl: "partials/home.html"
   });
   $stateProvider.state('sign-in',{
     url:"/sign-in",
